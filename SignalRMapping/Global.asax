43b3e459c3483de87a5d2ca8e9c2d12f3cc5810b
Phil <peva0411@gmail.com>
2013-01-18 23:19:32 -0500

    Initial Commit

---
+++ SignalRMapping/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRMapping.MvcApplication" Language="C#" %>
---
+++ SignalRMapping/Hubs/MapUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRMapping.Hubs
+{
+    public class MapUpdate : Hub
+    {
+        public void UpdateMapMessage(string message)
+        {
+            Clients.All.displayMapMessage(message);
+        }
+
+        public void UpdateMapExtent(double xMin, double yMin, double xMax, double yMax)
+        {
+            Clients.Others.setMapExtent(xMin, yMin, xMax, yMax);
+        }
+
+    }
+
+}
